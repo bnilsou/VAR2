690a9aabb366dc1f17bcc6e5a1f808e9764b6145
NilsBRLT <nils.burlat@gmail.com>
2017-10-26 13:52:26 +0200

    Initial commit

---
+++ VAR2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  VAR2
+//
+//  Created by Nils Burlat on 26/10/2017.
+//  Copyright © 2017 Nils Burlat. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, VAR2!\n";
+    return 0;
+}
